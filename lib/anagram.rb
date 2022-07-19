a60be78174668c1cabea97654fe8688d9d5e7acf
jordan <jordjamestaylor@gmail.com>
2022-07-19 15:32:39 -0400

    commit tree 4f148e5a55ca6fe8ec15bff1ca68bf42d15aace5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,33 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+    attr_accessor :word, :matching
+
+    def initialize(word)
+        @word = word
+        @matching = []
+        self.match(%w[enlists google inlets banana])
+    end
+
+    def match(array)
+        array.map do | pos_ana |
+            anagram = pos_ana.chars.join
+            anagram.each_char do | character |
+                if @word.include? character
+                    matching << character
+                end
+            end
+        end
+    end
+end
+
+#Send word to class - method invocation
+#Send word to class method - method invocation within initialize method
+#Break word into single characters - shallow loop 
+#Go over array and pull out each possible anagram - shallow loop
+#Break possible anagram into single characters - deep loop
+#Go over array of possible anagrams character by character
+    #Once a letter from the word doesn't match the letter in the array (at it's current index) move onto the next word
+    #If all letters matched in both the word and the current word in the array, return that word 
+
+listen = Anagram.new("listen")
